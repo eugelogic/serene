4c4c47b5d7651343ae16fd25137c04197d4303bd
Eugene Molari <eugelogic@gmail.com>
2021-08-09 20:52:49 +0100

    Add Discogs serverless function, fetch & dotenv

---
+++ netlify/functions/discogs.js
@@ -0,0 +1,15 @@
+// require('dotenv').config()
+const dotenv = require('dotenv')
+const fetch = require('node-fetch')
+
+exports.handler = async () => {
+    const DISCOGS_API = `https://api.discogs.com/database/search?genre=Jazz&year=2021&token=${process.env.DISCOGS_TOKEN}`
+
+    const response = await fetch(DISCOGS_API)
+    const data = await response.json()
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data)
+    }
+}
